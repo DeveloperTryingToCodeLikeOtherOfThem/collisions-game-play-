fe318fa53c557d72800ff9621ee3ebfeed7d62d2
a012 <Aeagan0507@gmail.com>
2025-09-06 20:23:01 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,272 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "treeImage":return img`
+................86..................
+...........6688867886...............
+...........8666877688868............
+............868777767768............
+.........688667777776688............
+........67767777777778666...........
+.........6776667767666868...........
+..........866667667677688...........
+.........8666666666667778...........
+........667766666666666676..........
+.......67766667666776667776.........
+......886667776676777666688.........
+.....67766777667767777666768........
+....6776666666777667776666776.......
+.....8667776667766676677776776......
+......8777666666667776777776688.....
+....6887766776677677777777776776....
+..8866666677767777777777766666778...
+.86666666777667767777766666776668...
+..88677666666777677677666667776668..
+..86776677666666666666667776666668..
+886666677766667666666776677766668...
+6668666676667766767767766677666668..
+88866666666777677677667666666776668.
+.86668866666766776776666667766666668
+.86688666666666776666667667776666688
+.668866666666666666666677666666688..
+..8866686666666666677667776666668...
+...866886666666666677667776666668...
+...86886668666666667666666666888....
+....88866886686666666666666668......
+......86886668666866668666868.......
+......88866688668866688866888.......
+........8888888688888ce868..........
+..............e88e88.ec.8...........
+...............eeee..e..............
+...............ceef.ce..............
+...............ceefcec..............
+...............feefce...............
+...............fceeec...............
+...............ffceec...............
+`;
+            case "image3":
+            case "invisibleCollision1":return img`
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+ffffffffffff
+`;
+            case "image4":
+            case "invisibleCollision2":return img`
+ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+`;
+            case "image5":
+            case "enemyInvisible":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image6":
+            case "yourSpriteImage":return img`
+. . . b 5 b . . . . . . . . . . 
+. . . . b 5 b . . . . . . . . . 
+. . . . . c b . . . . . . . . . 
+. . . . b b b b b b . . . . . . 
+. . . b 5 5 5 5 5 b b . . . . . 
+. . f d 5 5 f 1 d 5 b b . . . . 
+. . c 4 d 5 f f 1 5 5 b . . . . 
+. . 4 4 d d b f d 5 5 b . . . . 
+b 4 4 4 4 4 5 5 5 d b b d d d b 
+. b 4 4 4 4 4 5 5 b 5 5 5 d b b 
+. . b 5 5 5 5 5 d 5 5 5 5 c d b 
+. b 5 5 5 5 5 5 b 5 5 d c d d c 
+. b 5 5 5 5 5 5 5 b c c d d b c 
+. b d 5 5 5 5 5 d d d d d d c . 
+. . b b 5 5 5 d d d d d b c . . 
+. . . b b c c c c c c c c . . . 
+`;
+            case "image2":
+            case "playerImage":return img`
+.......ff...............
+....ffff2ff.............
+..ffeeeef2ff............
+.ffeeeeef22ff...........
+.feeeeffeeeef...........
+.fffffee2222ef..........
+fffe222ffffe2f..........
+ffffffffeeefff..........
+fefe44ebf44eef..........
+.fee4d4bfddef...........
+..feee4dddee.c..........
+...f2222eeddeccccccc....
+...f444e44ddecddddd.....
+...fffffeeee.ccccc......
+..ffffffff...c..........
+..fff..ff...............
+........................
+........................
+........................
+........................
+........................
+........................
+........................
+........................
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,106 @@
+info.setLife(5)
+
+namespace obstacles {
+    export class Obstacle {
+        parent: Sprite
+        ox: number
+        oy: number
+
+        constructor(parent: Sprite, ox: number, oy: number) {
+            this.parent = parent
+            this.ox = ox
+            this.oy = oy
+        }
+
+        detectObstacle(parent: Sprite, ox: number, oy: number, object: Sprite) {
+            game.onUpdate(() => {
+                // make when if the obstacle is left of the object and right of the object detecting a obstacle
+                if (parent.overlapsWith(object) && parent.x <= object.x) {
+                    parent.x -= ox
+                } else if (parent.overlapsWith(object) && parent.x >= object.x) {
+                    parent.x += ox
+                }
+                // for when it is up or down of the object 
+                if (parent.overlapsWith(object) && parent.y <= object.y) {
+                    parent.y -= oy
+                } else if (parent.overlapsWith(object) && parent.y >= object.y) {
+                    parent.y += oy
+                }
+            })
+        }
+
+    }
+
+    export function createObstacle(parent: Sprite, ox: number, oy: number): Obstacle {
+        const detectObstacle = new Obstacle(parent, ox, oy)
+        return detectObstacle
+    }
+}
+
+let player = sprites.create(assets.image`playerImage`, SpriteKind.Player)
+player.setStayInScreen(true)
+let myTree: Sprite = null
+game.onUpdateInterval(randint(1000, 1500), () => {
+    myTree = sprites.create(assets.image`treeImage`)
+    myTree.vx = randint(-50, 50)
+    // scene.cameraFollowSprite(myTree)
+    obstacle.detectObstacle(player, 2, 2, myTree)
+}) 
+const obstacle = obstacles.createObstacle(player, 1, 1)
+controller.moveSprite(player) 
+scene.cameraFollowSprite(player)
+let invisiblecollision = sprites.create(assets.image`invisibleCollision1`)
+invisiblecollision.setPosition(screen.width / 4, 60)
+invisiblecollision.setFlag(SpriteFlag.Invisible, true)
+obstacle.detectObstacle(player, 10, 0, invisiblecollision)
+let invisiblecollision2 = sprites.create(assets.image`invisibleCollision2`)
+invisiblecollision2.setPosition(80, screen.height * 2 - 120)
+invisiblecollision2.setFlag(SpriteFlag.Invisible, true)
+// invisiblecollision2.setFlag(SpriteFlag.RelativeToCamera, true)
+invisiblecollision2.setFlag(SpriteFlag.StayInScreen, true)
+obstacle.detectObstacle(player, 0, 10, invisiblecollision2)
+// player.follow(invisiblecollision2)        
+let yourSprite: Sprite = null 
+yourSprite = sprites.create(assets.image`enemyInvisible`, SpriteKind.Enemy)
+
+
+forever(() => {
+    if(player.y < config.DISPLAY_HEIGHT / 4) {
+        player.y += 5
+    }
+}) 
+
+game.onUpdate(() => {
+    info.changeScoreBy(control.timer1.seconds())
+})
+
+game.onUpdateInterval(randint(2000, 4000), () => {
+   setTimeout(() => {
+        yourSprite = sprites.create(assets.image`yourSpriteImage`, SpriteKind.Enemy)
+       yourSprite.setPosition(player.x + 10, player.y)
+       yourSprite.vx = -randint(20, 40)
+   }, 2000)
+}) 
+
+game.onUpdate(() => {
+    player.sayText("enemy spawning at " + yourSprite.x + yourSprite.y, 500, false)
+})
+
+game.onUpdate(() => {
+    yourSprite.sayText("player is at" + player.x + player.y, 500, false)
+})
+
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, sprite => {
+    pause(1000)
+    info.changeLifeBy(-1)
+    sprite.say("you have" + " " + info.life() + " left", 1000)
+}) 
+
+info.onLifeZero(() => {
+    game.showLongText("your previous seconds of survival is " + info.score() + " and saved data for position is " + player.x + player.y, DialogLayout.Center)
+    pauseUntil(() => controller.A.isPressed())
+    control.reset()
+}) 
+
+// tell the user why the seconds can be underfined 
+game.showLongText("How to play this game, avoid the ducks and the trees are collision obstacle hitboxes that can make you block to hit other side because it has hitboxes bumping to you, good luck.", DialogLayout.Center)
